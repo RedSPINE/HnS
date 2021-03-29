5f08edfb91877efd01847acfcdb8af52688a1256
RedSPINE <rc.redspine@gmail.com>
2021-03-29 20:09:08 +0200

    :construction: Hitboxes VFX Attacks

--- Assets/Scripts/Skills/SkillSO.cs
+++ Assets/Scripts/Skills/SkillSO.cs
@@ -15,19 +15,38 @@ public abstract class SkillSO : ScriptableObject
 
     [HideInInspector] public float skillDuration = -1;
     protected Vector3 direction;
-    public Vector3 Direction {
+    public Vector3 Direction
+    {
         get => direction;
     }
 
     [System.Serializable]
-    public struct CancelWindow { 
+    public struct CancelWindow
+    {
         [Tooltip("If this parameter is true, the skill can only be canceled by a dodge.")]
         public bool dodgeOnly;
-        [Range(0,1)] public float start;
-        [Range(0,1)] public float end;
+        [Range(0, 1)] public float start;
+        [Range(0, 1)] public float end;
     }
+
+    [System.Serializable]
+    public class HitBox
+    {
+        [Range(0, 1)] public float timeToFire;
+        [Range(0, 1)] public float timeToHit;
+        public bool noParent;
+        public GameObject prefab;
+        private GameObject gameObject;
+        public GameObject Go { get => gameObject; set => gameObject = value; }
+        private bool instanciated;
+        public bool IsInstanciated { get => instanciated; set => instanciated = value; }
+        private bool hit;
+        public bool Hit { get => hit; set => hit = value; }
+    }
+
     [SerializeField] protected CancelWindow[] cancelWindows;
-    
+    [SerializeField] protected HitBox[] hitBoxes;
+
     protected float internalCounter = 0;
 
     // Implement the State Pattern
@@ -38,8 +57,8 @@ public abstract class SkillSO : ScriptableObject
     public virtual void Enter(PlayerController controller)
     {
         internalCounter = 0;
-        skillDuration = animation.length/animationSpeed;
-        if (lookCursor) 
+        skillDuration = animation.length / animationSpeed;
+        if (lookCursor)
             direction = controller.LookCursor();
         else
             direction = controller.Direction;
@@ -52,11 +71,37 @@ public abstract class SkillSO : ScriptableObject
         internalCounter += Time.deltaTime;
         var dist = displacement.Evaluate(internalCounter / skillDuration) * Time.deltaTime * 100;
         controller.Move(direction.normalized * displacement.Evaluate(internalCounter / skillDuration) * Time.deltaTime / skillDuration);
+
+        foreach (HitBox hitBox in hitBoxes)
+        {
+            if (hitBox.IsInstanciated && hitBox.Hit) continue;
+            if (internalCounter >= hitBox.timeToHit && hitBox.IsInstanciated && !hitBox.Hit)
+            {
+                hitBox.Hit = true;
+                // TODO: create a hitbox and apply effects damage
+            }
+            if (!hitBox.IsInstanciated && internalCounter >= hitBox.timeToFire)
+            {
+                hitBox.IsInstanciated = true;
+                Quaternion rotation = hitBox.prefab.transform.rotation;
+                rotation.eulerAngles.Set(rotation.x, controller.transform.rotation.y, 0);
+                Instantiate(hitBox.prefab, controller.transform.position + hitBox.prefab.transform.position, rotation, controller.transform);
+            }
+        }
+
         OnUpdate(controller);
     }
 
     public virtual void Quit(PlayerController controller)
     {
+        Debug.Log("OnQuit !");
+        foreach (HitBox hitBox in hitBoxes)
+        {
+            Destroy(hitBox.Go);
+            hitBox.Go = null;
+            hitBox.IsInstanciated = false;
+            hitBox.Hit = false;
+        }
         return;
     }
 
@@ -64,15 +109,11 @@ public abstract class SkillSO : ScriptableObject
     {
         foreach (CancelWindow cancelWindow in cancelWindows)
         {
-            if(!dodge && cancelWindow.dodgeOnly) continue;
-            if(cancelWindow.start < normalizedTime && normalizedTime < cancelWindow.end) return true;
+            if (!dodge && cancelWindow.dodgeOnly) continue;
+            if (cancelWindow.start < normalizedTime && normalizedTime < cancelWindow.end) return true;
         }
         return false;
     }
 
-    public struct HitBox
-    {
-        public float timeToFire;
-        public GameObject prefab;
-    }
+
 }
