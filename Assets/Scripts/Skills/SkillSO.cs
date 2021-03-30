94f0ad832a325a1c9065ec1d193347499dc1afb7
RedSPINE <rc.redspine@gmail.com>
2021-03-30 14:49:40 +0200

    :construction: VFX + skills

--- Assets/Scripts/Skills/SkillSO.cs
+++ Assets/Scripts/Skills/SkillSO.cs
@@ -34,7 +34,7 @@ public abstract class SkillSO : ScriptableObject
     {
         [Range(0, 1)] public float timeToFire;
         [Range(0, 1)] public float timeToHit;
-        public bool noParent;
+        public bool destroyOnQuit;
         public GameObject prefab;
         private GameObject gameObject;
         public GameObject Go { get => gameObject; set => gameObject = value; }
@@ -69,23 +69,32 @@ public abstract class SkillSO : ScriptableObject
     public virtual void SkillUpdate(PlayerController controller)
     {
         internalCounter += Time.deltaTime;
-        var dist = displacement.Evaluate(internalCounter / skillDuration) * Time.deltaTime * 100;
-        controller.Move(direction.normalized * displacement.Evaluate(internalCounter / skillDuration) * Time.deltaTime / skillDuration);
+
+        float normalizedTime = internalCounter/skillDuration;
+        float dist = displacement.Evaluate(normalizedTime) * Time.deltaTime;
+        controller.Move(direction.normalized * dist);
 
         foreach (HitBox hitBox in hitBoxes)
         {
+            // Don’t need to instantiate or hit ? → Continue
             if (hitBox.IsInstanciated && hitBox.Hit) continue;
-            if (internalCounter >= hitBox.timeToHit && hitBox.IsInstanciated && !hitBox.Hit)
-            {
-                hitBox.Hit = true;
-                // TODO: create a hitbox and apply effects damage
-            }
-            if (!hitBox.IsInstanciated && internalCounter >= hitBox.timeToFire)
+            // Instantiate if needed
+            if (!hitBox.IsInstanciated && normalizedTime >= hitBox.timeToFire)
             {
                 hitBox.IsInstanciated = true;
                 Quaternion rotation = hitBox.prefab.transform.rotation;
-                rotation.eulerAngles.Set(rotation.x, controller.transform.rotation.y, 0);
-                Instantiate(hitBox.prefab, controller.transform.position + hitBox.prefab.transform.position, rotation, controller.transform);
+                rotation.eulerAngles.Set(rotation.x, 0, rotation.y);
+                Vector3 position = controller.transform.position + hitBox.prefab.transform.position;
+                // Instantiate(hitBox.prefab, hitBox.prefab.transform.position, rotation, controller.transform);
+                hitBox.Go = Instantiate(hitBox.prefab, controller.transform, false);
+                continue;
+            }
+            // Hit if needed
+            if (normalizedTime >= hitBox.timeToHit && hitBox.IsInstanciated && !hitBox.Hit)
+            {
+                hitBox.Hit = true;
+                // TODO: create a hitbox and apply effects damage
+                continue;
             }
         }
 
@@ -97,7 +106,8 @@ public abstract class SkillSO : ScriptableObject
         Debug.Log("OnQuit !");
         foreach (HitBox hitBox in hitBoxes)
         {
-            Destroy(hitBox.Go);
+            if (hitBox.destroyOnQuit) Destroy(hitBox.Go);
+            else if (hitBox.Go != null) hitBox.Go.transform.transform.SetParent(null);
             hitBox.Go = null;
             hitBox.IsInstanciated = false;
             hitBox.Hit = false;
